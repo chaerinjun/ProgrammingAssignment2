71220f813cf8cec8191c94e6303135af5e7c2c70
chaerinjun <47540021+chaerinjun@users.noreply.github.com>
2020-07-26 00:10:41 -0400

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,23 @@
+makeCacheMatrix <- function(x = array()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
+cacheSolve <- function(x, ...) {
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
+}
